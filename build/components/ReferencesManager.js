eae860f4e57a21a9b8d730e30330737c58949e87
Robin de Mourat <robin.demourat@gmail.com>
2017-05-11 15:57:40 +0200

    update build

--- build/components/ReferencesManager.js
+++ build/components/ReferencesManager.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
 var _keys = require('babel-runtime/core-js/object/keys');
 
 var _keys2 = _interopRequireDefault(_keys);
@@ -42,13 +46,13 @@ var CSL = require('citeproc');
 var HtmlToReactParser = require('html-to-react').Parser;
 var htmlToReactParser = new HtmlToReactParser();
 
-var Bibliography = function (_Component) {
-  (0, _inherits3.default)(Bibliography, _Component);
+var ReferencesManager = function (_Component) {
+  (0, _inherits3.default)(ReferencesManager, _Component);
 
-  function Bibliography(props) {
-    (0, _classCallCheck3.default)(this, Bibliography);
+  function ReferencesManager(props) {
+    (0, _classCallCheck3.default)(this, ReferencesManager);
 
-    var _this = (0, _possibleConstructorReturn3.default)(this, (Bibliography.__proto__ || (0, _getPrototypeOf2.default)(Bibliography)).call(this, props));
+    var _this = (0, _possibleConstructorReturn3.default)(this, (ReferencesManager.__proto__ || (0, _getPrototypeOf2.default)(ReferencesManager)).call(this, props));
 
     _this.state = {
       bibliography: '',
@@ -65,19 +69,52 @@ var Bibliography = function (_Component) {
     _this.makeReactBibliography = function (processor, items) {
       processor.updateItems((0, _keys2.default)(items));
       var bibResults = processor.makeBibliography();
+
       var biblioStr = bibResults[1].join('\n');
       return htmlToReactParser.parse(biblioStr);
+    };
+
+    _this.makeReactCitations = function (processor, cits) {
+      return cits.reduce(function (inputCitations, citationData) {
+        var citations = (0, _extends3.default)({}, inputCitations);
+        var citation = citationData[0];
+        var citationsPre = citationData[1];
+        var citationsPost = citationData[2];
+        var citationObjects = processor.processCitationCluster(citation, citationsPre, citationsPost);
+        citationObjects = citationObjects[1];
+        citationObjects.forEach(function (cit) {
+          var order = cit[0];
+          var html = cit[1];
+          var ThatComponent = htmlToReactParser.parse(cit[1]);
+          var citationId = cit[2];
+          citations[citationId] = {
+            order: order,
+            html: html,
+            Component: ThatComponent
+          };
+        });
+        return citations;
+      }, {});
     };
     return _this;
   }
 
-  (0, _createClass3.default)(Bibliography, [{
+  (0, _createClass3.default)(ReferencesManager, [{
+    key: 'getChildContext',
+    value: function getChildContext() {
+      return {
+        bibliography: this.state.bibliography,
+        citations: this.state.citations
+      };
+    }
+  }, {
     key: 'componentDidMount',
     value: function componentDidMount() {
       if (this.props.locale && this.props.style) {
         var processor = new CSL.Engine(this.state.sys, this.props.style);
         this.setState({
           processor: processor,
+          citations: this.props.citations && this.makeReactCitations(processor, this.props.citations),
           bibliography: this.props.items && this.makeReactBibliography(processor, this.props.items)
         });
       }
@@ -97,9 +134,10 @@ var Bibliography = function (_Component) {
     value: function componentDidUpdate(prevProps) {
       var _this2 = this;
 
-      if (this.state.processor && this.props.items !== prevProps.items || !this.state.bibliography) {
+      if (this.state.processor && this.props.items !== prevProps.items || this.props.citations !== prevProps.citations || !this.state.bibliography) {
         this.setState({
-          bibliography: this.makeReactBibliography(this.state.processor, this.props.items) 
+          citations: this.props.citations && this.makeReactCitations(this.state.processor, this.props.citations),
+          bibliography: this.makeReactBibliography(this.state.processor, this.props.items)
         });
       }
       if (this.props.locale !== prevProps.locale) {
@@ -114,6 +152,7 @@ var Bibliography = function (_Component) {
         var processor = new CSL.Engine(sys, this.props.style);
         this.setState({
           processor: processor,
+          citations: this.props.citations && this.makeReactCitations(processor, this.props.citations),
           bibliography: this.makeReactBibliography(processor, this.props.items),
           sys: sys
         });
@@ -122,6 +161,7 @@ var Bibliography = function (_Component) {
         var _processor = new CSL.Engine(this.state.sys, this.props.style);
         this.setState({
           processor: _processor,
+          citations: this.props.citations && this.makeReactCitations(_processor, this.props.citations),
           bibliography: this.makeReactBibliography(_processor, this.props.items)
         });
       }
@@ -129,35 +169,33 @@ var Bibliography = function (_Component) {
   }, {
     key: 'render',
     value: function render() {
-      var _props$componentClass = this.props.componentClass,
-          componentClass = _props$componentClass === undefined ? 'Bibliography' : _props$componentClass;
-      var bibliography = this.state.bibliography;
+      var _props = this.props,
+          componentClass = _props.componentClass,
+          children = _props.children;
 
 
-      if (bibliography) {
-        return _react2.default.createElement(
-          'section',
-          { className: componentClass },
-          bibliography
-        );
-      } else {
-        return _react2.default.createElement(
-          'section',
-          { className: componentClass + ' loading' },
-          this.props.loadingContent || 'loading'
-        );
-      }
+      return _react2.default.createElement(
+        'section',
+        { className: componentClass },
+        children
+      );
     }
   }]);
-  return Bibliography;
+  return ReferencesManager;
 }(_react.Component);
 
-Bibliography.propTypes = {
+ReferencesManager.propTypes = {
   componentClass: _propTypes2.default.string,
   style: _propTypes2.default.string,
   locale: _propTypes2.default.string,
-  items: _propTypes2.default.object
+  items: _propTypes2.default.object,
+  citations: _propTypes2.default.array
 };
 
-exports.default = Bibliography;
+ReferencesManager.childContextTypes = {
+  bibliography: _propTypes2.default.array,
+  citations: _propTypes2.default.object
+};
+
+exports.default = ReferencesManager;
 module.exports = exports['default'];
--- build/index.js
+++ build/index.js
@@ -3,12 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Bibliography = undefined;
+exports.ReferencesManager = exports.Bibliography = undefined;
 
 var _Bibliography = require('./components/Bibliography');
 
 var _Bibliography2 = _interopRequireDefault(_Bibliography);
 
+var _ReferencesManager = require('./components/ReferencesManager');
+
+var _ReferencesManager2 = _interopRequireDefault(_ReferencesManager);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Bibliography = exports.Bibliography = _Bibliography2.default;
+var ReferencesManager = exports.ReferencesManager = _ReferencesManager2.default;
